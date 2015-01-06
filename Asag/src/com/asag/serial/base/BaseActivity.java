2598a59dc709c9f07760feea5c4c2ec40616b442
soulline <tf.zhao@zuche.com>
2015-01-06 10:37:06 +0800

    fix a bug
    fix a bug

--- Asag/src/com/asag/serial/base/BaseActivity.java
+++ Asag/src/com/asag/serial/base/BaseActivity.java
@@ -51,7 +51,7 @@ public class BaseActivity extends FragmentActivity {
 	public void setFullScreen() {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 
 	public void showToast(final String text) {
